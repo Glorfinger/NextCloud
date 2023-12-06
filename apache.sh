4649ff50f1e5c627504e0a3bde72cb8c0f541214
Etienne Chamarier <e.chamarier@agiirnetwork.com>
2023-12-06 15:53:42 +0100

    ajout de fonctionnalité sur installer apache

--- apache.sh
+++ apache.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
+connexion ssh
+verification des sources avec apt 
+met a jour le référenciel avec apt update
+install apache2
+sécurise le serveur web avec du https 
+
+# Vérifier si l'utilisateur est connecté en SSH
+if [ -n "$SSH_CONNECTION" ]; then
+    echo "Vous êtes connecté en SSH."
+else    
+    # Demande des identifiants de connexion
+
+read -p "SSH IP adress : " ip_adresse
+read -p "SSH Port : " ssh_port
+read -p "SSH Username : " username
+    
+# Connexion SSH (le mot de passe sera demandé lors de la connexion)
+ssh "$username"@"$server_address"
+fi
+
+# Vérification des sources avec apt
+sudo apt-get update
+
+# Mise à jour du référentiel avec apt update
+sudo apt-get upgrade -y
+
+# Installation d'Apache2
+sudo apt-get install apache2 -y
+
+# Sécurisation du serveur web avec HTTPS
+sudo a2enmod ssl
+sudo a2ensite default-ssl
+sudo service apache2 restart
+
+
+
+
+
+
+
+
+#!/bin/bash
+
 # Function to install Apache based on the package manager
 install_apache() {
     if [[ -n $(command -v apt-get) ]]; then
@@ -35,38 +78,3 @@ if [[ -n $(command -v systemctl) ]]; then
 else
     sudo service apache2 status || sudo service httpd status
 fi
-
-
-
-
-#!/bin/bash
-
-connexion ssh
-verification des sources avec apt 
-met a jour le référenciel avec apt update
-install apache2
-sécurise le serveur web avec du https 
-
-
-#!/bin/bash
-
-# Demander les informations de connexion SSH
-read -p "Entrez votre nom d'utilisateur SSH : " username
-read -p "Entrez l'adresse du serveur : " server_address
-
-# Connexion SSH (le mot de passe sera demandé lors de la connexion)
-ssh "$username"@"$server_address"
-
-# Vérification des sources avec apt
-sudo apt-get update
-
-# Mise à jour du référentiel avec apt update
-sudo apt-get upgrade -y
-
-# Installation d'Apache2
-sudo apt-get install apache2 -y
-
-# Sécurisation du serveur web avec HTTPS
-sudo a2enmod ssl
-sudo a2ensite default-ssl
-sudo service apache2 restart
