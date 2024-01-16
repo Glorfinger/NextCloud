188e19c85c8a4bcdd20e3107ca336456db5a43f5
EtienneChamarier <126485152+EtienneChamarier@users.noreply.github.com>
2024-01-16 16:15:57 +0100

    Update execute_script.sh

--- script/execute_script.sh
+++ script/execute_script.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+# Chemin vers le dossier contenant les scripts
+script_folder="/chemin/vers/votre/dossier/script"
+
+# Exécution des scripts dans l'ordre
+echo "Exécution du script : apache.sh"
+bash "$script_folder/apache.sh"
+
+echo "Exécution du script : hardening.sh"
+bash "$script_folder/hardening.sh"
+
+echo "Exécution du script : ufw.sh"
+bash "$script_folder/ufw.sh"
