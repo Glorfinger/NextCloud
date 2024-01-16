9c595db3814b727acebfb88346416a7fa0506c50
EtienneChamarier <126485152+EtienneChamarier@users.noreply.github.com>
2024-01-16 16:33:02 +0100

    Update execute_script.sh

--- script/execute_script.sh
+++ script/execute_script.sh
@@ -5,10 +5,10 @@ script_folder="~/bashScript/NextCloud/script"
 
 # Exécution des scripts dans l'ordre
 echo "Exécution du script : apache.sh"
-sudo bash "$script_folder/apache.sh"
+bash "$script_folder/apache.sh"
 
 echo "Exécution du script : hardening.sh"
-sudo bash "$script_folder/hardening.sh"
+bash "$script_folder/hardening.sh"
 
 echo "Exécution du script : ufw.sh"
-sudo bash "$script_folder/ufw.sh"
+bash "$script_folder/ufw.sh"
