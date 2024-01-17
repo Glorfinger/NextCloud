28bb7a4f989cab060597c5f2402dd732cfcc2300
EtienneChamarier <126485152+EtienneChamarier@users.noreply.github.com>
2024-01-17 16:21:53 +0100

    Update ufw.sh

--- script/ufw.sh
+++ script/ufw.sh
@@ -1,5 +1,63 @@
 #!/bin/bash
 
+# Installer UFW
+echo "Installation d'UFW en cours..."
+sudo apt update
+sudo apt install ufw -y
+
+# Activer UFW
+echo "Activation d'UFW..."
+sudo ufw enable
+
+# Demander les ports pour SSH, HTTP et HTTPS
+read -p "Veuillez entrer le port SSH que vous souhaitez utiliser (par défaut 22) : " ssh_port
+read -p "Veuillez entrer le port HTTP que vous souhaitez utiliser (par défaut 80) : " http_port
+read -p "Veuillez entrer le port HTTPS que vous souhaitez utiliser (par défaut 443) : " https_port
+
+# Vérifier si les ports sont vides, si c'est le cas, affecter les valeurs par défaut.
+ssh_port=${ssh_port:-22}
+http_port=${http_port:-80}
+https_port=${https_port:-443}
+
+# Modifier le fichier de configuration SSH
+echo "Modification du fichier de configuration SSH..."
+sudo sed -i "s/^\(Port \)[0-9]\+/\1$ssh_port/" /etc/ssh/sshd_config
+
+# Modifier le fichier de configuration Apache pour HTTP
+echo "Modification du fichier de configuration Apache pour HTTP..."
+sudo sed -i "s/^\(<VirtualHost \*\):\([0-9]\+\)/\1:$http_port/" /etc/apache2/sites-enabled/000-default.conf
+
+# Modifier le fichier de configuration Apache pour HTTPS
+echo "Modification du fichier de configuration Apache pour HTTPS..."
+sudo sed -i "s/^\(<VirtualHost \*\):\([0-9]\+\)/\1:$https_port/" /etc/apache2/sites-enabled/default-ssl.conf
+
+# Recharger la configuration SSH
+echo "Rechargement de la configuration SSH..."
+sudo systemctl reload ssh
+
+# Recharger la configuration Apache
+echo "Rechargement de la configuration Apache..."
+sudo systemctl reload apache2
+
+# Autoriser le trafic SSH
+echo "Autorisation du trafic SSH..."
+sudo ufw allow $ssh_port/tcp
+
+# Autoriser le trafic HTTP
+echo "Autorisation du trafic HTTP..."
+sudo ufw allow $http_port/tcp
+
+# Autoriser le trafic HTTPS
+echo "Autorisation du trafic HTTPS..."
+sudo ufw allow $https_port/tcp
+
+echo "Configuration d'UFW terminée."
+
+
+
+
+
+: << 'COMMENT'
 # Fonction pour afficher une barre de progression
 progress_bar() {
     local duration=${1}
@@ -74,4 +132,5 @@ sudo ufw allow $https_port/tcp | progress_bar 1 "Autorisation du trafic HTTPS"
 
 echo "Configuration d'UFW terminée."
 
+COMMENT
 
