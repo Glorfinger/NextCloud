24e1f1c04789dc492ae8d0077e0fe40da05a917b
EtienneChamarier <126485152+EtienneChamarier@users.noreply.github.com>
2024-01-17 16:16:52 +0100

    Update hardening.sh

--- script/hardening.sh
+++ script/hardening.sh
@@ -9,6 +9,46 @@
 
 #!/bin/bash
 
+echo
+echo "Sauvegarde du fichier de configuration existant..."
+sudo cp /etc/ssh/sshd_config /etc/ssh/ssh_config_backup
+
+echo "Création du répertoire sshd_config.d..."
+if [ ! -d "/etc/ssh/sshd_config.d" ]; then
+    sudo mkdir -p /etc/ssh/sshd_config.d
+    echo "Include /etc/ssh/sshd_config.d/*.conf" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+fi
+
+echo "Création du fichier hardening dans /etc/ssh/sshd_config.d..."
+sudo touch /etc/ssh/sshd_config.d/hardening.conf
+sudo chown root:root /etc/ssh/sshd_config.d/hardening.conf
+sudo chmod 644 /etc/ssh/sshd_config.d/hardening.conf
+sudo sshd -t
+
+# Configuration du fichier hardening.conf
+echo 'PermitRootLogin no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'MaxAuthTries 3' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'LoginGraceTime 20' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'PermitEmptyPasswords no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'PasswordAuthentication no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'ChallengeResponseAuthentication no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'KerberosAuthentication no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'X11Forwarding no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'PermitUserEnvironment no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+echo 'DebianBanner no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
+
+# Redémarrage du service SSH
+sudo service ssh restart
+
+echo "Installation et configuration d'OpenSSH terminées"
+echo
+echo
+echo
+echo
+
+
+: << 'COMMENT'
+
 # Fonction pour afficher une ligne de séparation
 print_separator() {
     echo "------------------------------------------------------------"
@@ -67,48 +107,8 @@ print_message "Installation et configuration d'OpenSSH terminées"
 print_separator
 
 
-
 : << 'COMMENT'
 
-echo
-echo "Sauvegarde du fichier de configuration existant..."
-sudo cp /etc/ssh/sshd_config /etc/ssh/ssh_config_backup
-
-echo "Création du répertoire sshd_config.d..."
-if [ ! -d "/etc/ssh/sshd_config.d" ]; then
-    sudo mkdir -p /etc/ssh/sshd_config.d
-    echo "Include /etc/ssh/sshd_config.d/*.conf" | sudo tee -a /etc/ssh/sshd_config > /dev/null
-fi
-
-echo "Création du fichier hardening dans /etc/ssh/sshd_config.d..."
-sudo touch /etc/ssh/sshd_config.d/hardening.conf
-sudo chown root:root /etc/ssh/sshd_config.d/hardening.conf
-sudo chmod 644 /etc/ssh/sshd_config.d/hardening.conf
-sudo sshd -t
-
-# Configuration du fichier hardening.conf
-echo 'PermitRootLogin no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'MaxAuthTries 3' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'LoginGraceTime 20' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'PermitEmptyPasswords no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'PasswordAuthentication no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'ChallengeResponseAuthentication no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'KerberosAuthentication no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'X11Forwarding no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'PermitUserEnvironment no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-echo 'DebianBanner no' | sudo tee -a /etc/ssh/sshd_config.d/hardening.conf > /dev/null
-
-# Redémarrage du service SSH
-sudo service ssh restart
-
-echo "Installation et configuration d'OpenSSH terminées"
-echo
-echo
-echo
-echo
-
-
-: << 'COMMENT'
 
 # Fonction pour afficher une barre de progression
 progress_bar() {
