c0e42eea5fb65bed2fe1a66b968ac58e3d1642c0
EtienneChamarier <126485152+EtienneChamarier@users.noreply.github.com>
2024-01-17 15:08:26 +0100

    Update apache.sh

--- script/apache.sh
+++ script/apache.sh
@@ -8,10 +8,27 @@ echo
 # Installation d'Apache2
 sudo apt-get install apache2 -y
 
+echo
+echo
+echo "Sécurisation du serveur web..."
+echo
+
 # Sécurisation du serveur web avec HTTPS
+echo
+echo "Activation du mode SSL"
+echo
 sudo a2enmod ssl 
+echo
+echo "Configuration de HTTPS"
+echo
 sudo a2ensite default-ssl 
+echo
+echo "Redémarrage d'Apache"
+echo
 sudo service apache2 restart 
+echo
+echo "Rechargement de la configuration d'Apache"
+echo
 sudo systemctl reload apache2 
 
 echo "Installation et configuration d'Apache terminées"
