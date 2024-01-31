eca776f01885926fe9226f1d7afcf90e3b210413
Joss Lenoir <120194459+Glorfinger@users.noreply.github.com>
2024-01-31 11:27:27 +0100

    Update nextcloud.sh

--- script/nextcloud.sh
+++ script/nextcloud.sh
@@ -1,45 +1,28 @@
 #!/bin/bash
 
-#Update and Upgrade the Ubuntu Packages
-sudo apt update && apt upgrade
+# Update package lists and install necessary packages
+sudo apt update
+sudo apt install -y mariadb-server libapache2-mod-php7.4 \
+    php7.4-common php7.4-mysql php7.4-xml php7.4-zip
 
-#install Apache and MySQL Server
-sudo apt install apache2 
+# Secure MariaDB installation
+sudo mysql_secure_installation
 
-#Install PHP and other Dependencies and Restart Apache
+# Download and extract Nextcloud
+sudo mkdir -p /var/www/html/nextcloud
+wget -P /tmp https://download.nextcloud.com/server/releases/latest.tar.bz2
+sudo tar -xjf /tmp/latest.tar.bz2 -C /var/www/html/
 
-sudo apt install libapache2-mod-php php-bz2 php-gd php-curl php-mbstring php-imagick php-zip php-ctype php-curl php-dom php-json php-posix php-bcmath php-xml php-intl php-gmp zip unzip wget
+# Set permissions
+sudo chown -R www-data:www-data /var/www/html/nextcloud/
 
-#Enable required Apache modules and restart Apache:
-sudo a2enmod rewrite dir mime env headers
-sudo systemctl restart apache2
+# Create a database and user for Nextcloud
+sudo mysql -e "CREATE DATABASE nextcloud;"
+sudo mysql -e "CREATE USER 'nextcloud'@'localhost' IDENTIFIED BY 'password';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES;"
 
-#Download and unzip at the web root (/var/www/html) folder
-cd /var/www/html
-wget https://download.nextcloud.com/server/releases/latest.zip
-unzip latest.zip
-
-#Move all nextcloud content to the web root (/var/www/html) folder
-cd /var/www/html/nextcloud
-sudo mv * .* ../
-
-#Remove empty nextcloud directory
-sudo rmdir /var/www/html/nextcloud
-
-#Change the ownership of the nextcloud content directory to the HTTP user.
-sudo chown -R www-data:www-data /var/www/html
-
-#Run the nextcloud installation CLI commands.
-cd /var/www/html
-sudo -u www-data php occ  maintenance:install
-
-#If everything goes well the command will output “Nextcloud was successfully installed”.
-
-#Now save the file and restart apache2
-
-sudo systemctl restart apache2
-
-#Now, Go to the Browser and type http:// [ ip or fqdn ] of the server, as the configuration is completed command line, the Login page will appear.
+echo "Nextcloud has been successfully installed."
 
 
 
