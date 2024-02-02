e94652be475e939dddf52e9e52f174b7268733ef
Joss Lenoir <120194459+Glorfinger@users.noreply.github.com>
2024-02-02 15:19:40 +0100

    Update nextcloud.sh

--- script/nextcloud.sh
+++ script/nextcloud.sh
@@ -1,30 +1,65 @@
 #!/bin/bash
 
-# Update package lists and install necessary packages
-sudo apt update
-sudo apt install -y mariadb-server libapache2-mod-php7.4 \
-    php7.4-common php7.4-mysql php7.4-xml php7.4-zip
+# Update package lists
+sudo apt-get update
+sudo apt-get upgrade
 
-# Secure MariaDB installation
+echo
+echo
+echo "Installation des packages en cours..."
+echo
+
+# Install necessary packages
+sudo apt-get install mariadb-server php8.2 php8.2-common php8.2-curl php8.2-gd php8.2-intl php8.2-mbstring php8.2-xmlrpc php8.2-mysql php8.2-xml php8.2-cli php8.2-zip
+sudo apt-get install wget unzip
+
+echo
+echo
+echo "Installation de Nextcloud en cours..."
+echo
+
+# Telecharge Nextcloud
+wget https://download.nextcloud.com/server/releases/latest.zip
+
+# Deplace Nextcloud au bon endroit
+echo
+cd /tmp
+echo
+sudo unzip latest.zip
+echo
+mv nextcloud/ /var/www/html/
+echo
+
+# Modifier les droits de Nextcloud pour Apache2
+echo
+chown -R www-data:www-data /var/www/html/nextcloud
+echo
+
+echo
+echo "Préparation de la base de donné en cours..."
+echo 
+
+# Securise mysql
+echo
 sudo mysql_secure_installation
+echo
 
-# Download and extract Nextcloud
-sudo mkdir -p /var/www/html/nextcloud
-wget -P /tmp https://download.nextcloud.com/server/releases/latest.tar.bz2
-sudo tar -xjf /tmp/latest.tar.bz2 -C /var/www/html/
+#Connexion à mysql
+echo
+mysql -u root -p
+echo
 
-# Set permissions
-sudo chown -R www-data:www-data /var/www/html/nextcloud/
-
-# Create a database and user for Nextcloud
-sudo mysql -e "CREATE DATABASE nextcloud;"
-sudo mysql -e "CREATE USER 'nextcloud'@'localhost' IDENTIFIED BY 'password';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost';"
-sudo mysql -e "FLUSH PRIVILEGES;"
-
+# Création de la database
+echo
+CREATE DATABASE db23nextcloud;
+echo
+GRANT ALL ON db23nextcloud.* TO 'usr23nextcloud'@'localhost' IDENTIFIED BY 'Password14';
+echo
+FLUSH PRIVILEGES;
+echo
+EXIT;
+echo
+echo
 echo "Nextcloud has been successfully installed."
 echo "Accédez à votre Nextcloud en utilisant l'adresse : http://$(hostname -I | awk '{print $1}')/nextcloud"
-
-
-
 
