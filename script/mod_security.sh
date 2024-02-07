a09d8e322217bf455c4178edd1f3e1141247339e
EtienneChamarier <126485152+EtienneChamarier@users.noreply.github.com>
2024-02-07 10:33:28 +0100

    Update mod_security.sh

--- script/mod_security.sh
+++ script/mod_security.sh
@@ -6,9 +6,6 @@ sudo apt-get install -y libapache2-mod-security2
 echo "Copie du fichier de configuration par défaut..."
 sudo cp /etc/modsecurity/modsecurity.conf-recommended /etc/apache2/modsecurity.conf
 
-echo "Configuration d'Apache pour charger le module ModSecurity..."
-sudo sh -c 'echo "Include modsecurity.conf" >> /etc/apache2/apache2.conf'
-
 echo "Redémarrage du service Apache pour appliquer les changements..."
 sudo service apache2 restart
 
